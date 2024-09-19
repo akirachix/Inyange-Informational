8eaaf0058ac85986af155be3781f98dcb59de25b
rehema_funani <rehemafunani@gmail.com>
2024-09-19 18:08:07 +0300

    adds files

--- buildmart-informational/src/app/page.tsx
+++ buildmart-informational/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Services from "./components/Services";
 import Team from "./components/Team";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return(
     <main>
       <Services/>
       <Team/>
+      <Footer/>
+
     </main>
   )
 }
