e0d3db60be3821d308b72d77079db65b11a2d1d3
student <faithnyamburawamui@gmail.com>
2024-09-21 15:24:04 +0300

    pulled dev

--- buildmart-informational/src/app/page.tsx
+++ buildmart-informational/src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Features from "./components/Features";
 import Services from "./components/Services";
 import Team from "./components/Team";
@@ -19,21 +18,4 @@ export default function Home() {
     </main>
   )
 }
-=======
-import Image from "next/image";
-import Navbar from "./Navbar";
-import Landingpage from "./Landingpage";
-import Head from 'next/head';
 
-
-export default function Home() {
-  return (
-      <main>
-        <Navbar/>
-        <Landingpage/>
-        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </main>
-     
-  );
-}
->>>>>>> 800d3254abe93cdf29813054aa206ce5427baa5a
